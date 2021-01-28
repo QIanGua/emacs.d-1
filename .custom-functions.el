ac1f64853995626a32dc2d3f90d9c1eb1eaffd7d
caicailiushui <15757826110@163.com>
2021-01-28 19:27:41 +0800

    update: interact with iTerm2

--- .custom-functions.el
+++ .custom-functions.el
@@ -52,10 +52,33 @@
   (message "Sucessfully opened in Finder")
   )
 
-(defun iterm-here ()
-  (interactive)
-  (dired-smart-shell-command "open -a iTerm $PWD" nil nil))
+;; (defun iterm-here ()
+;;   (interactive)
+;;   (dired-smart-shell-command "open -a iTerm $PWD" nil nil))
 
+(defun iterm-focus ()
+  (interactive)
+  (do-applescript
+   " do shell script \"open -a iTerm\"\n"
+   ))
+
+
+(defun iterm-goto-filedir-or-home ()
+  "Go to present working dir and focus iterm"
+  (interactive)
+  (do-applescript
+   (concat
+	" tell application \"iTerm2\"\n"
+	"   tell the current session of current window\n"
+	(format "     write text \"cd %s\" \n"
+			;; string escaping madness for applescript
+			(replace-regexp-in-string "\\\\" "\\\\\\\\"
+									  (shell-quote-argument (or default-directory "~"))))
+	"   end tell\n"
+	" end tell\n"
+	" do shell script \"open -a iTerm\"\n"
+	))
+  )
 
 (defun evil-replace-sr()
   (interactive)
